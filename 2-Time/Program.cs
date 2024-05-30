68c1983cfe3bf485754130885cc7c071bec8a6ef
Mopano <99732517+funnypar@users.noreply.github.com>
2024-05-31 00:00:23 +0330

    Time
    In this project , i have learned more about "if statements" in C# and that was amazing 😃

---
+++ 2-Time/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace HelloWorld {
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Give space at the start
+            Console.WriteLine();
+            // Get the input from user
+            Console.Write("Enter thr time : ");
+            string userInput = Console.ReadLine();
+            int time = Convert.ToInt32(userInput);
+            // Conditions
+            if (time < 0 || time > 24)
+            {
+                Console.WriteLine("Invalid Time");
+            }
+            else if (time >= 0 && time <= 5) 
+            {
+                Console.WriteLine("Dawn");
+            }
+            else if (time >= 6 && time <= 11) 
+            {
+                Console.WriteLine("Morning");
+            }
+            else if (time >= 12 && time <= 17) 
+            {
+                Console.WriteLine("Afternoon");
+            }
+            else 
+            {
+                Console.WriteLine("Evening");
+            }
+            Console.WriteLine();
+        }
+    }
+}

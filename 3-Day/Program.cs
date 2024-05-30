50884c3b80399c8b73c19f016c0324dc7e8e0033
Mopano <99732517+funnypar@users.noreply.github.com>
2024-05-31 00:19:35 +0330

    Day
    In this project, i have learned about " switch case" in C#  👑

---
+++ 3-Day/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace HelloWorld {
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Give space at the start
+            Console.WriteLine();
+            // Get the input from user
+            Console.Write("Enter The Number Of A Day : ");
+            int day = Convert.ToInt32(Console.ReadLine());
+            // Conditions
+            switch (day)
+            {
+                case 1 :
+                    Console.WriteLine("Today Is Monday");
+                    break;
+                case 2 :
+                    Console.WriteLine("Today Is Sunday");
+                    break;
+                case 3 :
+                    Console.WriteLine("Today Is Tuesday");
+                    break;
+                case 4 :
+                    Console.WriteLine("Today Is Wednesday");
+                    break;
+                case 5 :
+                    Console.WriteLine("Today Is Thursday");
+                    break;
+                case 6 : 
+                    Console.WriteLine("Today Is Friday");
+                    break;
+                case 7 :
+                    Console.WriteLine("Today Is Saturday");
+                    break;
+                default:
+                    Console.WriteLine("There are only 7 days in a week :) ");
+                    break;
+            }
+            Console.WriteLine();
+        }
+    }
+}

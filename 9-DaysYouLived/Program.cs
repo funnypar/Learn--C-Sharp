979b29f1f57f1da86fcc648842765c0369725aed
Mopano <99732517+funnypar@users.noreply.github.com>
2024-06-01 18:25:48 +0330

    DaysYouLived
    In this project, we calculate the days that we have lived 🥳

---
+++ 9-DaysYouLived/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace HelloWorld {
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+           // Give space at the start
+            Console.WriteLine();
+            // Get the input from user
+            Console.Write("Enter Your Age : ");
+            int age;
+            bool success = int.TryParse(Console.ReadLine(), out age);
+            Console.WriteLine();
+            // Conditions
+            while (!success || age < 1 || age > 125)
+            {
+                Console.Write("Please Enter Just A Positive Number : ");
+                success = int.TryParse(Console.ReadLine(), out age);
+            }
+            // calculations
+            Console.WriteLine("You Are Living " + age * 12 * 31 + " Days 🥳");
+            Console.WriteLine();
+        }
+    }
+}

eb2fd2e788665ffe06ddae4b0b402a188b2e9828
Mopano <99732517+funnypar@users.noreply.github.com>
2024-06-01 18:15:43 +0330

    FizzBuzz
    Let's play FizzBuzz 🥳

---
+++ 8-FizzBuzz/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace HelloWorld {
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+           // Give space at the start
+            Console.WriteLine();
+            // Get the input from user
+            Console.Write("Enter A Number : ");
+            int num;
+            bool success = int.TryParse(Console.ReadLine(), out num);
+            Console.WriteLine();
+            // Conditions
+            while (!success || num < 0)
+            {
+                Console.Write("Please Enter Just A Positive Number : ");
+                success = int.TryParse(Console.ReadLine(), out num);
+            }
+            // calculations
+            for (int i = 1; i <= num; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0) 
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else 
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}

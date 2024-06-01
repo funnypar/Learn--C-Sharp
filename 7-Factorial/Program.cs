d634c33d00303c1a86fdfc147391adbb8a342456
Mopano <99732517+funnypar@users.noreply.github.com>
2024-06-01 17:51:22 +0330

    Factorial
    This is just a project for calculate the factorial 🤓

---
+++ 7-Factorial/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace HelloWorld {
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+           // Give space at the start
+            Console.WriteLine();
+            // Get the input from user
+            Console.Write("Enter A Number : ");
+            int num;
+            bool success = int.TryParse(Console.ReadLine(), out num);
+            // Conditions
+            while (!success || num < 0)
+            {
+                Console.Write("Please Enter Just A Positive Number : ");
+                success = int.TryParse(Console.ReadLine(), out num);
+            }
+            // calculations
+            if (num == 0 || num == 1) 
+            {
+                Console.WriteLine();
+                Console.WriteLine("The Factorial is : 1");
+                Console.WriteLine();
+            }
+            else 
+            {
+                int result = 1;
+                for (int i = num; i > 0; i-- )
+                {
+                    result *= i;
+                }
+                Console.WriteLine();
+                Console.WriteLine("The Factorial is : " + result);
+                Console.WriteLine();
+            }
+        }
+    }
+}

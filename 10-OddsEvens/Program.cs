d62d1e8949f8b55aeb36ace7cf10b49106d75761
Mopano <99732517+funnypar@users.noreply.github.com>
2024-06-01 18:41:49 +0330

    OddsEvens
    In this project, we will logs odds and evens separately 😎

---
+++ 10-OddsEvens/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace HelloWorld {
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+           // Give space at the start
+            Console.WriteLine();
+            // Get the input from user
+            Console.Write("Enter A Number : ");
+            int num;
+            bool success = int.TryParse(Console.ReadLine(), out num);
+            Console.WriteLine();
+            // Conditions
+            while (!success || num < 0)
+            {
+                Console.Write("Please Enter Just A Positive Number : ");
+                success = int.TryParse(Console.ReadLine(), out num);
+            }
+            // calculations
+            // evens
+            Console.WriteLine("Evens Are : ");
+            Console.WriteLine();
+            for (int i = 0; i <= num ; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("-------------------------------------------------------");
+            Console.WriteLine();
+
+            // odds
+            Console.WriteLine("Odds Are : ");
+            Console.WriteLine();
+            for (int i = 0; i <= num ; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}

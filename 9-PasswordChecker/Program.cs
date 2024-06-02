de878eddd5e9eb8aab0fc91fcb522b6f1e3e2254
Mopano <99732517+funnypar@users.noreply.github.com>
2024-06-02 15:19:51 +0330

    PasswordChecker
    In this project, i used some methods that i have learned about "strings" ✌️

---
+++ 9-PasswordChecker/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace HelloWorld {
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+           // Give space at the start
+            Console.WriteLine();
+            // Get the input from user
+            Console.Write("Password : ");
+            string pass = Console.ReadLine();
+            Console.WriteLine();
+
+            while (string.IsNullOrEmpty(pass) || pass.StartsWith(" "))
+            {
+                Console.Write("Please Start Password With Words : ");
+                pass = Console.ReadLine();
+            }
+
+            pass = pass.Trim().Replace(" ", "");
+
+            // Get password again
+
+            Console.Write("Enter Password Again : ");
+            string pass2 = Console.ReadLine();
+            Console.WriteLine();
+            while (string.IsNullOrEmpty(pass2))
+            {
+                Console.Write("Please Start Password With Words : ");
+                pass2 = Console.ReadLine();
+            }
+
+            pass2 = pass2.Trim().Replace(" ", "");
+
+            // conditions
+            Console.WriteLine(pass == pass2 ? "Passwords Match 🤌" : "Passwords Do Not Match 🤏");
+            Console.WriteLine();
+        }
+    }
+}

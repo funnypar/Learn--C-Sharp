c580e1a8885067b9bbeead532c1aa7e8ac9bc1b1
Mopano <99732517+funnypar@users.noreply.github.com>
2025-01-22 17:28:20 +0330

    Mosh Practice 8

---
+++ Mosh Practice 8/Practice8/Program.cs
@@ -0,0 +1,15 @@
+﻿using Practice9;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var stack = new Stack();
+        stack.Push(1);
+        stack.Push(2);
+        stack.Push(3);
+        Console.WriteLine(stack.Pop());
+        Console.WriteLine(stack.Pop());
+        Console.WriteLine(stack.Pop());
+    }
+}
---
+++ Mosh Practice 8/Practice8/Stack.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice9
+{
+    public class Stack
+    {
+        private List<object> _stack = new List<object>();
+        public void Push(object Obj)
+        {
+            if (Obj == null) throw new InvalidOperationException("The Entry was not valid!");
+
+            _stack.Add(Obj);
+        }
+
+        public object Pop()
+        {
+            var result = _stack[_stack.Count - 1];
+            _stack.RemoveAt(_stack.Count-1);
+            return result;
+        }
+
+        public void Clear()
+        {
+            _stack.Clear();
+            Console.WriteLine("Stack has been cleared!");
+        }
+    }
+}

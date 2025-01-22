2d80b9db6460766377171a5474eb65ce30b63d65
Mopano <99732517+funnypar@users.noreply.github.com>
2025-01-22 17:25:11 +0330

    Mosh Practice 7

---
+++ Mosh Practice 7/Practice8/Post.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice8
+{
+    public class Post
+    {
+        private int _like = 0;
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public int LikeCount { get { return _like; } }
+        public DateTime CreatedOn { get; } = DateTime.Now;
+
+
+
+        public void Like()
+        {
+            _like += 1;
+        }
+        public void DisLike()
+        {
+            if (_like > 0)
+                _like -= 1;
+            else Console.WriteLine("The Like numbers is 0 ");
+
+        }
+    }
+}
---
+++ Mosh Practice 7/Practice8/Program.cs
@@ -0,0 +1,34 @@
+﻿using Practice8;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var post = new Post();
+        post.Like();
+        post.Like();
+        post.Like();
+        post.Like();
+        post.Like();
+
+        Console.WriteLine(post.LikeCount);
+
+        post.DisLike();
+
+
+
+
+        post.DisLike();
+        post.DisLike();
+        post.DisLike();
+        post.DisLike();
+        post.DisLike();
+        post.DisLike();
+        post.DisLike();
+        post.DisLike();
+        post.DisLike();
+
+        Console.WriteLine(post.LikeCount);
+
+    }
+}
